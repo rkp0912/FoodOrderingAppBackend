dc7e8b8ec85b5ce6f49fa5e82912f5fea50075c4
E532650 <Krishna.Praveen@Honeywell.com>
2020-05-25 10:31:49 +0530

    Adding DB entities and relationships for all endpoints

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryEntity.java
@@ -0,0 +1,25 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    @Size(max=200)
+    private String uuid;
+
+    @Column(name = "category_name")
+    @NotNull
+    @Size(max=255)
+    private String categoryName;
+}
---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantEntity.java
@@ -0,0 +1,99 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name="restaurant")
+public class RestaurantEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    @Size(max=200)
+    private String uuid;
+
+    @Column(name = "restaurant_name")
+    @NotNull
+    @Size(max=50)
+    private String restaurantName;
+
+    @Column(name = "photo_url")
+    @NotNull
+    @Size(max=255)
+    private String photoUrl;
+
+    @Column(name = "customer_rating")
+    @NotNull
+    private Float customerRating;
+
+    @Column(name = "average_price_for_two")
+    @NotNull
+    private Integer averagePriceForTwo;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private AddressEntity addressEntity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Float getCustomerRating() {
+        return customerRating;
+    }
+
+    public void setCustomerRating(Float customerRating) {
+        this.customerRating = customerRating;
+    }
+
+    public Integer getAveragePriceForTwo() {
+        return averagePriceForTwo;
+    }
+
+    public void setAveragePriceForTwo(Integer averagePriceForTwo) {
+        this.averagePriceForTwo = averagePriceForTwo;
+    }
+
+    public AddressEntity getAddressEntity() {
+        return addressEntity;
+    }
+
+    public void setAddressEntity(AddressEntity addressEntity) {
+        this.addressEntity = addressEntity;
+    }
+}
